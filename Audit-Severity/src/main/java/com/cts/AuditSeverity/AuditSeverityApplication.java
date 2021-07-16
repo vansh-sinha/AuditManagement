4fe59e81d319cb0333a6fac2d8f640d1003783d8
vansh-sinha <47246060+vansh-sinha@users.noreply.github.com>
2021-07-16 16:40:01 +0530

    Update AuditSeverityApplication.java

--- Audit-Severity/src/main/java/com/cts/AuditSeverity/AuditSeverityApplication.java
+++ Audit-Severity/src/main/java/com/cts/AuditSeverity/AuditSeverityApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * 
- * POD6 -Audit Management System
+ * POD3 -Audit Management System
  * This is Application class for Spring boot
  *
  */
